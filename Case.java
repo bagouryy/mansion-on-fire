d1fba9d5dbef652851c47b36cbea085521e001c0
Nour Albagoury <nour.albagoury@universite-paris-saclay.fr>
2023-11-27 13:47:20 +0000

    ajout des fichiers

---
+++ Case.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public abstract class Case {
+        public final int lig, col;
+        public Case(int l, int c) {
+            this.lig = l;
+            this.col = c;
+        }
+        public abstract boolean estTraversable();
+}
