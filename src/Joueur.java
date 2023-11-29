0b31014c6ba62143e46a7c109ed911fa0518bf40
Nour Albagoury <nour.albagoury@universite-paris-saclay.fr>
2023-11-29 14:31:50 +0100

    change de cles => keys ajout d'un bool sortie et fonction exit et estsortie

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,18 +1,20 @@
 public class Joueur {
     private CaseTraversable c;
     private int resistance;
-    private int cles;
+    private int keys;
+    private boolean sortie;
+
     public Joueur(CaseTraversable c, int r, int k) {
         this.c = c;
         this.resistance = r;
-        this.cles = k;
+        this.keys = k;
     }
 
     public void bouge(Case cible) {
         if(cible instanceof Porte && !((Porte) cible).isOpen()){
-            if(cles >= 1){
+            if(keys >= 1){
                 ((Porte) cible).ouvrePorte();
-                cles--;
+                keys--;
             }
         }
 
@@ -32,5 +34,21 @@ public class Joueur {
     public CaseTraversable getCase(){
         return c;
     }
+
+    public void burn(int chaleur){
+        resistance -= chaleur;
+    }
+
+    public void takeKey(){this.keys++;}
+
+    public void exit(){
+        c.vide();
+        sortie =true;
+    }
+
+    public boolean estSortie(){return sortie;}
+
+
+
 
 }
