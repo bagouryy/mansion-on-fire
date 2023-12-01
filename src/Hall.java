dbbc813740fb8cc7cff735499fe028fe5bff3ebe
Nour Albagoury <nour.albagoury@universite-paris-saclay.fr>
2023-12-01 01:18:00 +0100

    added apple and constructor for apple and getter

--- src/Hall.java
+++ src/Hall.java
@@ -1,5 +1,6 @@
 public class Hall extends CaseTraversable {
 	private boolean key;
+	private boolean apple;
 	public Hall(int l, int c){
 		super(l, c);
 	}
@@ -14,19 +15,25 @@ public class Hall extends CaseTraversable {
 		this.chaleur = chaleur;
 	}
 
-	public Hall(int l, int c, boolean key) {
+	public Hall(int l, int c, boolean key, boolean apple) {
 		super(l,c);
 		this.key = key;
+		this.apple = apple;
 	}
 
-	public Hall(int l, int c, int chaleur, boolean key) {
+	public Hall(int l, int c, int chaleur, boolean key, boolean apple) {
 		super(l,c);
 		this.key = key;
 		this.chaleur = chaleur;
+		this.apple = apple;
 	}
 
 	public boolean containsKey(){
 		return key;
+	}
+
+	public boolean containsApple(){
+		return apple;
 	}
 
 	@Override
